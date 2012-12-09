a98d6e52098e8c46eac0359956864bfcb18d333d
Yhnavein <floubadour@gmail.com>
2012-12-09 18:19:09 +0100

    init commit with Twitter Bootstrap in version 2.2.2
    also included: KnockoutJS 2.1.0, jQuery DataTables, Bootstrap
    Datepicker, Date.js, jQuery validationEngine, and localization files for
    them

---
+++ OverrideTheAdmin.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Routing;
+using Orchard.Themes;
+using Orchard.UI.Admin;
+
+namespace BootstrapAdminTheme
+{
+    public class OverrideTheAdmin : IThemeSelector
+    {
+        public ThemeSelectorResult GetTheme(RequestContext context)
+        {
+            if (AdminFilter.IsApplied(context))
+            {
+                return new ThemeSelectorResult { Priority = 110, ThemeName = "BootstrapAdminTheme" };
+            }
+
+            return null;
+        }
+    }
+
+}
